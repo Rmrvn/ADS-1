4deb7b46190a6b6d9a18c28cd795268af1902b49
Rmrvn <160429541+Rmrvn@users.noreply.github.com>
2024-03-03 23:22:09 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,16 +5,60 @@
 
 bool checkPrime(uint64_t value) {
 // вставьте код функции
+  if (value <= 1) {
+    return false;
+}
+if (value <= 3) {
+    return true;
+}
+
+if (value % 2 == 0 || value % 3 == 0) {
+    return false;
+}
+
+for (int i = 5; i * i <= value; i += 6) {
+    if (value % i == 0 || value % (i + 2) == 0) {
+        return false;
+    }
+}
+
+return true;
 }
 
 uint64_t nPrime(uint64_t n) {
 // вставьте код функции
+   if (n == 1) {
+     return 2;
+ }
+ uint64_t cnt = 1;
+ uint64_t nummer = 3;
+ while (cnt < n) {
+     if (checkPrime(nummer)) {
+         cnt++;
+     }
+     if (cnt < n) {
+         nummer += 2;
+     }
+ }
+ return nummer;
 }
 
 uint64_t nextPrime(uint64_t value) {
 // вставьте код функции
+   uint64_t nummer = value + 1;
+if (!checkPrime(nummer)) {
+    num += 1;
+}
+return nummer;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
 // вставьте код функции
+  uint64_t sumch;
+for (uint64_t i = 2; i < hbound; i++) {
+    if (checkPrime(i)) {
+        sumch += i;
+    }
+}
+return sumch;
 }
